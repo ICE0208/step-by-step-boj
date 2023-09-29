1bdd5d6d159a0de0182ee953575446382ca92229
ICE0208 <doky3307@naver.com>
2023-09-29 12:10:27 +0900

    Add Code: 1912 in Java

---
+++ 23.동적 계획법 1/1912.연속합/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        int result = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            result = Math.max(result, dp[i]);
+        }
+
+        bw.write(String.valueOf(result));
+        bw.close();
+        br.close();
+    }
+}
