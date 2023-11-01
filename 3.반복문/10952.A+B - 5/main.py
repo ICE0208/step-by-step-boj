9f2d8944fdbb93852056643e470743c32a126ea0
ICE0208 <doky3307@naver.com>
2023-11-01 22:54:10 +0900

    Init: 10952

---
+++ 3.반복문/10952.A+B - 5/main.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    while True:
+        a, b = map(int, input().split())
+        if a == b == 0:
+            break
+        print(a + b)
