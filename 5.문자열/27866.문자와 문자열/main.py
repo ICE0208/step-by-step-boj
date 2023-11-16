873422c8d3bc0e3581c7d068514a0b47a34f0e07
ICE0208 <doky3307@naver.com>
2023-11-16 20:51:00 +0900

    Init: 27866

---
+++ 5.문자열/27866.문자와 문자열/main.py
@@ -0,0 +1,11 @@
+print(input()[int(input()) - 1])
+
+# import sys
+
+# input = sys.stdin.readline
+
+# if __name__ == "__main__":
+#     S = input().rstrip()
+#     i = int(input())
+
+#     print(S[i - 1])
