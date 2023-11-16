39417cc0b3a29878a901bc1b2813bd539fb924f4
ICE0208 <doky3307@naver.com>
2023-11-16 20:58:24 +0900

    Init: 2743

---
+++ 5.문자열/2743.단어 길이 재기/main.py
@@ -0,0 +1,12 @@
+print(len(input()))
+
+# import sys
+
+# input = sys.stdin.readline
+
+# if __name__ == "__main__":
+#     S = input().rstrip()
+#     cnt = 0
+#     for s in S:
+#         cnt += 1
+#     print(cnt)
